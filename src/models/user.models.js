9ce79553c1f414a73a72077635aed1ba7c8bf82b
Suraj Telrandhe <suraj.telrandhe16@gmail.com>
2024-12-03 14:30:47 +0530

    adding tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt frfom""jsonwebtoken"
 
 const userSchema = new Schema(
   {
@@ -46,5 +48,41 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 )
+
+userSchema.pre("save", async function (next) {
+
+  if (!this.modefied("password")) return next()
+
+  this.password = bcrypt.hash(this.password, 10)
+
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await abcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+  // short lived access token
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullname: this.fullname
+  },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+  // Long lived access token
+  return jwt.sign({
+    _id: this._id,
+  },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  )
+}
 
 export const User = mongoose.model("User", userSchema)
